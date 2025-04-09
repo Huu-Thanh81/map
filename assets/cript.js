2bac780edd0ba322db2c0a28366975065cea00f0
Bùi Hữu Thành <buithanh812004@gmail.com>
2025-04-09 17:44:22 +0700

    version-5

--- assets/cript.js
+++ assets/cript.js
@@ -15,6 +15,7 @@ const customuser = L.icon({
 });
 //*Câu 1: Xây dựng bản đồ các điểm bán hàng có sẵn
 const storeLocations = [
+  { name: "user", position: [10.05119, 105.77371] }, //vị trí người đang đứng
   { name: "Cửa hàng 1", position: [10.0191, 105.7819] },
   { name: "Cửa hàng 2", position: [9.994, 105.71646] },
   { name: "Cửa hàng 3", position: [10.06504, 105.7592] },
@@ -24,58 +25,59 @@ const storeLocations = [
 // Thêm các điểm bán hàng lên bản đồ
 //*Câu 2:
 var standinPoint;
-storeLocations.forEach(function (store) {
-  const marker = L.marker(store.position, { icon: customIcon })
-    .addTo(map)
-    .bindPopup(
-      `Tọa độ ${store.name}:<br/> (${store.position[0]}, ${store.position[1]})`
-    );
-  marker.on("click", () => {
-    L.Routing.control({
-      waypoints: [L.latLng(standinPoint._latlng.lat, standinPoint._latlng.lng), L.latLng(store.position)],
-      createMarker: function (i, waypoint, n) {
-        return null; // không tạo marker nào cả (ẩn luôn icon mặc định)
-      },
-    })
-      .on("routesfound", function (e) {
-        e.routes[0].coordinates.forEach((item, index) => {
-          setTimeout(() => {
-            standinPoint.setLatLng([item.lat, item.lng]);
-          }, index * 30);
-        });
-        console.log(e);
-      })
-      .addTo(map);
-  });
+storeLocations.forEach(function (item) {
+  if (item.name === "user") {
+    standinPoint = L.marker(item.position, { icon: customuser })
+      .addTo(map)
+      .bindPopup(`Điểm đang đứng`);
+    funHover(standinPoint);
+  } else {
+    const marker = L.marker(item.position, { icon: customIcon })
+      .addTo(map)
+      .bindPopup(
+        `Tọa độ ${item.name}:<br/> (${item.position[0].toFixed(3)}, ${item.position[1].toFixed(3)})`
+      );
+    funHover(marker);
+    marker.on("click", () => {
+      L.Routing.control({
+        waypoints: [L.latLng([10.05119, 105.77371]), L.latLng(item.position)],
+        createMarker: function (i, waypoint, n) {
+          return null; // không tạo marker nào cả (ẩn luôn icon mặc định)
+        },
+      }).on("routesfound", function (e) {
+          e.routes[0].coordinates.forEach((item, index) => {
+            setTimeout(() => {
+              standinPoint.setLatLng([item.lat, item.lng]);
+            }, index * 20);
+          });
+        })
+        .addTo(map);
+    });
+  }
 });
 //*Câu 3, 4, 5:
 // Chọn hai điểm bất kỳ trên bản đồ
 let coordContainer = document.getElementById("coordinates");
 let points = [];
+let ArrayPInit = [];
 let PointerInit;
 let count = 0;
 map.on("click", (e) => {
-    if(!standinPoint){
-    standinPoint = L.marker([e.latlng.lat, e.latlng.lng], { icon: customuser })
-    .addTo(map)
-    .bindPopup(`Điểm đang đứng`);
-    funHover(standinPoint);
-    return;
-  }
   points.push(e.latlng);
   ++count;
   poiAdd(e.latlng.lat, e.latlng.lng);
+  AppHTML(points);
   funUpdate();
-  AppHTML();
+  zoom();
 });
-const funHover = (pointer) => {
+function funHover(pointer) {
   pointer.on("mouseover", function () {
     this.openPopup();
   });
   pointer.on("mouseout", function () {
     this.closePopup();
   });
-};
+}
 const poiAdd = (lat, lng) => {
   PointerInit = L.marker([lat, lng], {
     icon: L.icon({
@@ -85,9 +87,15 @@ const poiAdd = (lat, lng) => {
   })
     .addTo(map)
     .bindPopup(
-      `Điểm ${count}<br/>Tọa độ: (${[lat.toFixed(2), lng.toFixed(2)]})`
+      `Điểm ${count}<br/>Tọa độ: (${[lat.toFixed(3), lng.toFixed(3)]})`
     );
-  funHover(PointerInit);
+  ArrayPInit.push(PointerInit);
+  for (let i = 0; i < ArrayPInit.length; ++i) {
+    funHover(ArrayPInit[i]);
+    ArrayPInit[i].on("click", () => {
+      AppHTML(ArrayPInit[i], i);
+    });
+  }
 };
 const funUpdate = () => {
   if (points.length >= 2) {
@@ -97,21 +105,28 @@ const funUpdate = () => {
       createMarker: function (i, waypoint, n) {
         return null; // không tạo marker nào cả (ẩn luôn icon mặc định)
       },
-    })
-    .addTo(map);
-    console.log(points)
+    }).addTo(map);
   }
 };
 //*Câu 7:
-const AppHTML = () => {
+const AppHTML = (poin, id) => {
   var chuoi = "";
-  points.forEach((item, index) => {
-    chuoi += `<tr>
+  try {
+    poin.forEach((item, index) => {
+      chuoi += `<tr id = "tr${index}">
     <td>${index + 1}</td>
-    <td>${item.lat.toFixed(2)}</td>
-    <td>${item.lng.toFixed(2)}</td>
+    <td>${item.lat.toFixed(3)}</td>
+    <td>${item.lng.toFixed(3)}</td>
   </tr>`;
-  });
+    });
+  } catch (error) {
+    chuoi += `<tr id = "tr${id}">
+    <td>${id + 1}</td>
+    <td>${poin._latlng.lat.toFixed(3)}</td>
+    <td>${poin._latlng.lng.toFixed(3)}</td>
+  </tr>`;
+  }
+
   var chuoixuli = `
           <table >
             <tr>
@@ -123,6 +138,24 @@ const AppHTML = () => {
           </table>`;
   coordContainer.innerHTML = chuoixuli;
 };
+function zoom() {
+  let listTr = [];
+  for(let i = 0; i<points.length; ++i){
+    let tr = document.querySelector(`#tr${i}`);
+    listTr.push(tr);
+    listTr[i].addEventListener("mouseover", () => {
+      map.flyTo(points[i], 15);
+      ArrayPInit[i]
+        .bindPopup(
+          `Điểm ${i + 1}<br/>Tọa độ: (${[
+            points[i].lat.toFixed(3),
+            points[i].lng.toFixed(3),
+          ]})`
+        )
+        .openPopup();
+    });
+  }
+}
 //*Câu 7:
 // Nhập tọa độ trực tiếp
 function addPoint(Lat, Lng) {
@@ -130,47 +163,55 @@ function addPoint(Lat, Lng) {
   ++count;
   poiAdd(Lat, Lng);
   funUpdate();
-  AppHTML();
+  AppHTML(points);
+  zoom();
 }
 const handleClick = () => {
   var valuelati = Number(document.querySelector(".container #lat").value);
   var valuelong = Number(document.querySelector(".container #lng").value);
   if (valuelati && valuelong) {
     addPoint(valuelati, valuelong);
+    document.querySelector(".container #lat").value = "";
+    document.querySelector(".container #lng").value = "";
   } else {
     alert("Không hợp lệ");
   }
 };
-const predefinedCoordinates = [
-  {coords: [10.031, 105.773] },
-  {coords: [10.025, 105.764] },
+const listSugges = [
+  { coords: [10.031, 105.773] },
+  { coords: [10.025, 105.764] },
+  { coords: [10.026, 105.748] },
 ];
 function showSuggestions() {
+  let flag = false;
   const input = document.getElementById("lat").value;
-  const suggestionsDiv = document.getElementById("latSuggestions");
-  suggestionsDiv.innerHTML = ""; // Xóa các gợi ý cũ
+  const sugDiv = document.getElementById("latSuggestions");
+  sugDiv.innerHTML = ""; // Xóa các gợi ý cũ
   if (input.trim() !== "") {
-      predefinedCoordinates.forEach(item => {
-          const value =  item.coords[0].toString();
-          if (value.includes(input)) {
-            suggestionsDiv .classList.add("suggestions");
-              const suggestionItem = document.createElement("div");
-              suggestionItem.className = "suggestion-item";
-              suggestionItem.innerText = `${item.coords[0]}, ${item.coords[1]}`;
-              // Thêm sự kiện click vào gợi ý
-              suggestionItem.onclick = function() {
-                  document.getElementById("lat").value = item.coords[0];
-                  document.getElementById("lng").value = item.coords[1];
-                  suggestionsDiv.innerHTML = ""; // Xóa gợi ý sau khi chọn
-                  suggestionsDiv .classList.remove("suggestions");
-              };
-             suggestionsDiv.appendChild(suggestionItem);
-            }
-          else {
-            suggestionsDiv .classList.remove("suggestions");
-          }
-      });
-  }
-  else
-   suggestionsDiv .classList.remove("suggestions");
+    listSugges.forEach((item) => {
+      const value = item.coords[0].toString();
+      if (value.includes(input)) {
+        flag = true;
+        sugDiv.classList.add("suggestions");
+        const suggesItem = document.createElement("div");
+        suggesItem.className = "suggestion-item";
+        suggesItem.innerText = `${item.coords[0]}, ${item.coords[1]}`;
+        // Thêm sự kiện click vào gợi ý
+        suggesItem.addEventListener("click", function () {
+          document.getElementById("lat").value = item.coords[0];
+          document.getElementById("lng").value = item.coords[1];
+          sugDiv.innerHTML = ""; // Xóa gợi ý sau khi chọn
+          sugDiv.classList.remove("suggestions");
+        });
+        suggesItem.addEventListener("mouseover", function () {
+          document.getElementById("lat").value = item.coords[0];
+          document.getElementById("lng").value = item.coords[1];
+        });
+        sugDiv.appendChild(suggesItem);
+      }
+    });
+    if (!flag) {
+      sugDiv.classList.remove("suggestions");
+    }
+  } else sugDiv.classList.remove("suggestions");
 }
